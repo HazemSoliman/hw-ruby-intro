fb75c75c778249ba7cf880fd91c21388ae0d7d09
Hazem Soliman <hazemsoliman811@gmail.com>
2019-09-25 03:00:51 +0200

    complete part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,25 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  arr.length.zero? ? 0 : arr.sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  arr.length.zero? ? 0 : arr.length == 1 ? arr[0] : (arr.sort[-1] + arr.sort[-2])
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  if arr.length < 2
+    false
+  else
+    arr.each_with_index do |ele, idx|
+      ((idx + 1)...arr.length).each do |element|
+        return true if (ele + arr[element]) == n
+      end
+    end
+  end
+  false
 end
 
 # Part 2
